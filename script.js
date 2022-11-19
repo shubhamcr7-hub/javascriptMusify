0134fdf200c3260670ee90edede6ee27e2804582
shubhamcr7-hub <banerjees974@gmail.com>
2022-11-19 18:21:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("welcome to musify")
 let songIndex=0;
 let audioElement=new Audio('drop-it-124014.mp3');
 let masterPlay=document.getElementById('masterPlay');
+let progressBar=document.getElementById('Progress');
 let songs=[
     {songname:"closer", filePath:"mp3\drop-it-124014.mp3" },
     {songname:"closer", filePath:"mp3\drop-it-124014.mp3" }
@@ -25,5 +26,18 @@ masterPlay.addEventListener('click',()=>{
 
     }
     
+})
+
+audioElement.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+    let songProgress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    console.log(songProgress);
+    progressBar.value=songProgress;
+    //console.log('progressBar');
+
+})
+
+progressBar.addEventListener('change',()=>{
+    audioElement.currentTime=progressBar.value*audioElement.duration/100;
 })
 
